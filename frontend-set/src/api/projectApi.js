864a52c9196116ffdb1dab433dc91500f5e29067
Sang-Myeong Lee <teamjuji5@naver.com>
2025-08-16 14:55:12 +0900

    #127 bugfix: 비 로그인 - 홈페이지 - 관심 프로젝트 썸네일 안보이는 문제 해결 & 썸네일 없을 경우 기본 이미지 띄우기

--- frontend-set/src/api/projectApi.js
+++ frontend-set/src/api/projectApi.js
@@ -1,3 +1,71 @@
 import api from '@/api'
 
 const BASE_URL = '/project'
+
+export const getProjects = async () => {
+    try {
+        const res = await api.get('/project/list', {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        })
+
+        const projectList = res.data
+
+        for (const project of projectList) {
+            if (project.images == undefined || project.images.length === 0) {
+                const tempImage = {
+                    imageType: 'Project',
+                    imageUrl: '/images/logo.png',
+                    postId: project.projectId,
+                    createdAt: '',
+                }
+
+                project.images = [tempImage]
+                project.thumbnailImage = tempImage
+                project.thumbnailUrl = tempImage.imageUrl
+            }
+        }
+
+        return projectList
+    } catch (error) {
+        console.error('프로젝트 리스트 조회 실패:', error)
+        throw error
+    }
+}
+
+export const getRecommendedProjects = async () => {
+    try {
+        const res = await api.get('/project/list/keyword', {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        })
+
+        const projectList = res.data
+
+        console.log('✅ getRecommendedProjects - projectList before:', projectList)
+
+        for (const project of projectList) {
+            if (project.images == undefined || project.images.length === 0) {
+                const tempImage = {
+                    imageType: 'Project',
+                    imageUrl: '/images/logo.png',
+                    postId: project.projectId,
+                    createdAt: '',
+                }
+
+                project.images = [tempImage]
+                project.thumbnailImage = tempImage
+                project.thumbnailUrl = tempImage.imageUrl
+            }
+        }
+
+        console.log('✅ getRecommendedProjects - projectList after:', projectList)
+
+        return projectList
+    } catch (error) {
+        console.error('관심 기반 프로젝트 리스트 조회 실패:', error)
+        throw error
+    }
+}
